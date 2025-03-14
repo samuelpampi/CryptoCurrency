018fd7a6ea04b1fd0250044241270d6546370a9d
samuelpampi <samuelpampillonroa@gmail.com>
2025-03-15 00:45:47 +0100

    se ha añadido el calculo del resultado y el intercambio de seleccion

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-### Crypto Icons repository 
-[text](https://github.com/ErikThiart/cryptocurrency-icons/tree/master/64)
+# 🪙 **Calculadora de Divisas y Criptomonedas**
 
-### Crypto Exchance API
-[text](https://www.coinlore.com/es/cryptocurrency-data-api)
+## Descripción
 
+Este proyecto es una **calculadora de conversiones de divisas y criptomonedas**. Permite a los usuarios elegir entre una variedad de criptomonedas y divisas, y obtener el valor de conversión actual.
 
-### Echance currency
-[text](https://www.exchangerate-api.com/)
+El objetivo principal de este proyecto es **practicar TypeScript** y trabajar con la manipulación de datos provenientes de APIs externas. La interfaz es limpia y fácil de usar.
 
-### Flags api
-[text](https://flagsapi.com/#countries)
+## Funcionalidades
+
+- **Conversión de criptomonedas a divisas:** Permite al usuario elegir una criptomoneda y una divisa, y ver cuánto vale esa criptomoneda en la divisa seleccionada.
+- **Conversión de divisas a criptomonedas:** También puedes ingresar una cantidad de una divisa, y ver cuántas criptomonedas puedes obtener por esa cantidad.
+
+## Tecnologías
+
+- **Frontend:**
+  - **HTML5** para la estructura.
+  - **CSS3** para los estilos visuales.
+  - **JavaScript/TypeScript** para la lógica y manipulación de datos.
+  - **Axios** para realizar peticiones HTTP y obtener datos de las APIs.
+
+- **APIs utilizadas:**
+  - [**Coinlore API**](https://www.coinlore.com/): Para obtener información en tiempo real sobre criptomonedas, incluyendo el precio actual de cada una.
+  - [**Exchangerate-API**](https://www.exchangerate-api.com/): Para obtener la tasa de cambio entre divisas tradicionales y la conversión entre ellas.
+  - [**Cryptocurrency-icons**](https://github.com/ErikThiart/cryptocurrency-icons/tree/master/64): Para obtener los iconos de las criptomonedas.
+  - [**Flags API**](https://flagsapi.com/#countries): Para obtener las imagenes de las banderas de cada pais
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,138 @@
 import axios from "axios";
 
-var criptos: { [key: string]: string } = {};
+//Variables de datos
+var criptos: Record<string, { name: string; id: number; price:number }> = {};
+var cantidad: number; //Cantidad total
+var currency: string; //Divisa seleccionada
+var criptomoneda: string; //Criptomoneda seleccionada
 const {currencies} = require('./utils.ts');
+var cripto_value: number; //Valor de la criptomoneda en USD
+var result: number; //Resultado final
+
+//Elementos del formulario
+const btn_calcular:HTMLButtonElement = document.getElementById("calculate") as HTMLButtonElement;
+const btn_inc:HTMLButtonElement = document.getElementById("btn_inc") as HTMLButtonElement;
+const btn_dec:HTMLButtonElement = document.getElementById("btn_dec") as HTMLButtonElement;
+const input_amount:HTMLInputElement = document.getElementById("cantidad") as HTMLInputElement;
 const selectCripto:HTMLSelectElement = <HTMLSelectElement>document.getElementById("select-crypto");
 const selectCurrency:HTMLSelectElement = <HTMLSelectElement>document.getElementById("select-currency");
+const result_div: HTMLParagraphElement = document.getElementById("result") as HTMLParagraphElement;
+//Cogemos los contenedores que queremos intercambiar (incluye img, nombre y select)
+const selectCoin:HTMLElement = document.getElementById("select-coin") as HTMLElement;
+const selectMoneda:HTMLElement = document.getElementById("select-moneda") as HTMLElement;
+const intercambiar: HTMLElement = document.getElementById("change") as HTMLElement;
+
+
+//---------------------------------------------------------------------
+
 
 //Función para obtener la criptomoneda seleccionada
 function obtenerCripto(event: Event){
     let selected_cripto = (event.target as HTMLSelectElement).value;
     let html_cripto_name = document.getElementById("crypto-name") as HTMLSpanElement;
-    html_cripto_name.innerText = criptos[selected_cripto];
-    console.log(selected_cripto);
+    let html_cripto_img = document.getElementById("crypto-logo") as HTMLImageElement;
+
+    //Actualizamos el nombre y la imagen del select
+    html_cripto_name.innerText = criptos[selected_cripto].name;
+    html_cripto_img.src = `https://raw.githubusercontent.com/ErikThiart/cryptocurrency-icons/refs/heads/master/64/${criptos[selected_cripto].name.toLowerCase().replace(/\s+/g, "-")}.png`;
 }
 
 //Función para obtener la divisa seleccionada
 async function obtenerDivisa(event: Event){
     let selected_currency = (event.target as HTMLSelectElement).value;
     let html_currency_name = document.getElementById("currency-name") as HTMLSpanElement;
+    let html_country_img = document.getElementById("country-logo") as HTMLImageElement;
 
+    //Actualizamos el nombre y la imagen del select
     let currency_names = await axios.get('https://v6.exchangerate-api.com/v6/cced7cb933f509ab441a2d98/codes');
     let currency_info = currency_names.data.supported_codes.find((currency: [string, string]) => currency[0] == selected_currency);
     html_currency_name.innerText = currency_info[1];
-    console.log(selected_currency);
+
+    html_country_img.src = `https://flagcdn.com/h60/${currencies[selected_currency]}.png`;
 }
 
+//Incrementamos el la cantidad
+function incrementar(event:any){
+    event.preventDefault();
+
+    input_amount.value = (parseInt(input_amount.value) + 1).toString();
+}
+
+//Decrementamos la cantidad
+function decrementar(event:any){
+    event.preventDefault();
+
+    if(parseInt(input_amount.value) > 1){ //Nunca será menor de 1
+        input_amount.value = (parseInt(input_amount.value) - 1).toString();
+    }
+}
+
+//Calculamos el resultado
+async function calcularResultado(){
+    cantidad = parseInt(input_amount.value);
+    currency = selectCurrency.value;
+    criptomoneda = selectCripto.value;
+
+    console.log(`Cantidad: ${cantidad} Criptomoneda: ${criptomoneda} Divisa: ${currency}`);
+
+    //Calcular el cambio de USD a la divisa seleccionada
+    let cripto_value_usd = criptos[criptomoneda].price; 
+    
+    if(selectCoin.classList.contains('order-1')){ //La cantidad es de criptomonedas
+        //Pasamos un USD a la divisa seleccionada
+        let exchange_rate = (await axios.get(`https://v6.exchangerate-api.com/v6/cced7cb933f509ab441a2d98/pair/USD/${currency}`)).data.conversion_rate;
+        result = (cantidad * cripto_value_usd) * exchange_rate; 
+        result_div.innerText = `${cantidad} ${criptomoneda} = ${result.toFixed(2)} ${currency}`;
+
+    } else if(selectMoneda.classList.contains('order-1')){ //La cantidad es de divisa
+        //Pasamos un la divisa seleccionada a USD
+        let exchange_rate = (await axios.get(`https://v6.exchangerate-api.com/v6/cced7cb933f509ab441a2d98/pair/${currency}/USD`)).data.conversion_rate;
+        result = (cantidad * exchange_rate) / cripto_value_usd; 
+        result_div.innerText = `${cantidad} ${currency} = ${result.toFixed(2)} ${criptomoneda}`;   
+    }
+
+    
+}
+
+
+//Funcion para intercambiar los selects
+function intercambiarSelects(event: any) {
+    event.preventDefault();
+
+    console.log("Intercambiando selects");
+
+    //Cambiar las clases para alterar el orden visual
+    /* La propiedad toggle intercambia clases, es decir, sie stá la elimina y si no esta la crea*/
+    selectCoin.classList.toggle('order-1');
+    selectCoin.classList.toggle('order-3');
+    
+    
+
+    selectMoneda.classList.toggle('order-1');
+    selectMoneda.classList.toggle('order-3'); 
+}
 
 //Rellenamos los selects de forma automatica
 async function obtenerDatos(): Promise<void> {
     //Llamamos a la API de cryptos para poder rellenar de forma automatica los selects
     try{
-        let cripto_codes = await axios.get('https://api.coinlore.net/api/tickers/?limit=30');
+        let cripto_codes = await axios.get('https://api.coinlore.net/api/tickers/?limit=20');
 
         let criptos_data:[String] = cripto_codes.data.data;
         //Recorremos el array con reduce para construir un diccionario de criptos con symbol:nombre
-        criptos = criptos_data.reduce((acumulador:Record<string,string>, cripto:any) => {
-            acumulador[cripto.symbol] = cripto.name;
+        criptos = criptos_data.reduce((acumulador:any, cripto:any) => {
+            acumulador[cripto.symbol] = {
+                name: cripto.name,
+                id: parseInt(cripto.id),
+                price: parseFloat(cripto.price_usd)
+            };
+
             return acumulador;
-        }, {});
         
+        }, {});
+
+        console.log(criptos);
+
         //Rellenamos los selects
         for(let cripto_code in criptos){
             let option:HTMLOptionElement = document.createElement('option');
@@ -59,6 +154,13 @@ async function obtenerDatos(): Promise<void> {
     } catch(error){
         console.log("Error en la solicitud: ", error);
     };
+
+    //Añadimos listeners
+    btn_calcular.addEventListener('click', calcularResultado);
+    btn_inc.addEventListener('click', incrementar);
+    btn_dec.addEventListener('click', decrementar);
+    intercambiar.addEventListener('click', intercambiarSelects);
+
     
 }
 
